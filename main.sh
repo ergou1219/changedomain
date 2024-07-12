6b07e4a8cf013e5b92a5032c2951400db49fe9d4
ergou <58904481+ergou1219@users.noreply.github.com>
2024-07-13 03:29:42 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 设置要替换的旧字符串和新字符串
+old_string="https:\/\/qoqcloud\.pub"
+new_string="https:\/\/ovocloud\.pub"
+
+# 替换文件中的字符串
+sed -i "s/$old_string/$new_string/g" /etc/V2bX/config.json
+
+# 重启v2bx服务
+v2bx restart
