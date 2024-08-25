d80ab4b8e51ad6416249283a7a0900f56d7e7d16
ergou <58904481+ergou1219@users.noreply.github.com>
2024-08-25 18:47:20 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 设置要替换的旧字符串和新字符串
-old_string="https:\/\/qoqcloud\.pub"
-new_string="https:\/\/ovocloud\.pub"
+old_string="https:\/\/ovocloud\.pub"
+new_string="https:\/\/ovocloud\.icu"
 
 # 替换文件中的字符串
 sed -i "s/$old_string/$new_string/g" /etc/V2bX/config.json
